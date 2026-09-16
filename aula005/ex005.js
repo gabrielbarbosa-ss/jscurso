55a762e32113aec3a631f3bd0a26fff27af8f9ea
Gabriel <gabrielbarbosasoares2007@gmail.com>
2026-09-16 16:57:17 -0300

    Create ex005.js

---
+++ aula005/ex005.js
@@ -0,0 +1,23 @@
+//Condicionais
+
+let velocidade = 80;
+
+console.log("Teste um");
+
+if (velocidade > 80){
+    console.log("Voce foi multado! Passou acima da velocidade permitida.\n")
+} else{
+    console.log("Ande sempre com cinto de segurança!\n")
+}
+
+velocidade +=20;
+
+console.log("Teste dois");
+
+if (velocidade > 80){
+    console.log("Voce foi multado! Passou acima da velocidade permitida.")
+} else{
+    console.log("Ande sempre com cinto de segurança!")
+}
+
+//Com apenas um if é condição simples, com if e else é condição composta
